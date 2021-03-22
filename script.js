fab6d94c45b8f51f27fe50aa979593a9f3f64f25
RonaldVilorio <ronvilorio@gmail.com>
2021-03-22 19:21:48 -0400

    planning calculate function

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hey")
+// 26 letters
+let obj = {
+  1: ["A", "J", "S"],
+  2: ["B", "K", "T"],
+  3: ["C", "L", "U"],
+  4: ["D", "M", "V"],
+  5: ["E", "N", "W"],
+  6: ["F", "O", "X"],
+  7: ["G", "P", "Y"],
+  8: ["H", "Q", "Z"],
+  9: ["I", "R"],
+};
+atob
+// capture user input
+let fullName = "Ronald Adolfo Vilorio";
+
+// translate each letter into numbers to sum up
+
+for (let i = 0; i <= fullName.length - 1; i++) {
+    
+}
