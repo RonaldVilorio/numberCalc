c987e4dbb25d25b03ad289098e447d90873bfcde
RonaldVilorio <ronvilorio@gmail.com>
2021-03-24 15:22:50 -0400

    captured name,birthday input

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log("hey");
 // 26 letters
 let obj = {
   1: ["A", "J", "S"],
@@ -12,22 +11,34 @@ let obj = {
   9: ["I", "R"],
 };
 // capture user input
-let fullName = "Ronald Adolfo Vilorio";
-fullName = fullName.replace(/\s/g, "").toUpperCase();
+// let fullName = "Ronald Adolfo Vilorio";
+// fullName = fullName.replace(/\s/g, "").toUpperCase();
 
 // translate each letter into numbers to sum up
-let numArr = [];
+// let numArr = [];
 
-for (let i = 0; i <= fullName.length - 1; i++) {
-  for (let j = 1; j <= Object.keys(obj).length; j++) {
-    if (obj[j].includes(fullName[i])) {
-      numArr.push(j);
-    } else {
-      null;
-    }
-  }
-}
+// for (let i = 0; i <= fullName.length - 1; i++) {
+//   for (let j = 1; j <= Object.keys(obj).length; j++) {
+//     if (obj[j].includes(fullName[i])) {
+//       numArr.push(j);
+//     } else {
+//       null;
+//     }
+//   }
+// }
 // sum up the array of numbers
-let nameNumber = numArr.reduce((sum, num) => sum + num);
-// life Path Number
+// let nameNumber = numArr.reduce((sum, num) => sum + num);
 
+// grab  the input of the user
+// add function to html buttton
+// now add an event listener to wait for user to click on button
+// first find the button
+
+const submitButton = document.querySelector("#submitButton");
+const birthday = document.querySelector("#birthday").value;
+const fullName = document.querySelector("#fullName").value;
+
+function calculate(e) {
+  e.preventDefault();
+
+}
