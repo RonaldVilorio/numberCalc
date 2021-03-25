aa7d898a86345932631a9d952f777a3114569c78
RonaldVilorio <ronvilorio@gmail.com>
2021-03-25 10:12:13 -0400

    placed code in functions

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// 26 letters
 let obj = {
   1: ["A", "J", "S"],
   2: ["B", "K", "T"],
@@ -10,47 +9,37 @@ let obj = {
   8: ["H", "Q", "Z"],
   9: ["I", "R"],
 };
-// capture user input
-// let fullName = "Ronald Adolfo Vilorio";
-// fullName = fullName.replace(/\s/g, "").toUpperCase();
-
-// translate each letter into numbers to sum up
-// let numArr = [];
-
-// for (let i = 0; i <= fullName.length - 1; i++) {
-//   for (let j = 1; j <= Object.keys(obj).length; j++) {
-//     if (obj[j].includes(fullName[i])) {
-//       numArr.push(j);
-//     } else {
-//       null;
-//     }
-//   }
-// }
-// sum up the array of numbers
-// let nameNumber = numArr.reduce((sum, num) => sum + num);
-
-// grab  the input of the user
-// add function to html buttton
-// now add an event listener to wait for user to click on button
-// first find the button
 
 const submitButton = document.querySelector("#submitButton");
-const birthday = document.querySelector("#birthday").value;
-const fullName = document.querySelector("#fullName").value;
-// bday = document.querySelector("#birthday").value;
-// +bday.split("-").reduce((sum,num)=>sum+num)
 
-let lifePathNum = birthday
-  .split("-")
-  .reduce((sum, num) => sum + num)
-  .split("")
-  .map((str) => +str)
-  .reduce((sum, num) => sum + num);
-if (lifePathNum !== 11 || lifePathNum !== 22) {
-  lifepathNum = "" + lifepathNum;
-  lifePathNum = +lifePathNum[0] + +lifePathNum[1];
+function lifePathCalc() {
+  const birthday = document.querySelector("#birthday").value;
+  let lifePathNum = birthday
+    .split("-")
+    .reduce((sum, num) => sum + num)
+    .split("")
+    .map((str) => +str)
+    .reduce((sum, num) => sum + num);
+  if (lifePathNum !== 11 || lifePathNum !== 22) {
+    lifepathNum = "" + lifepathNum;
+    lifePathNum = +lifePathNum[0] + +lifePathNum[1];
+  }
 }
 
+function nameNumberCalc() {
+  let fullName = document.querySelector("#fullName").value;
+  fullName = fullName.replace(/\s/g, "").toUpperCase();
+
+  for (let i = 0; i <= fullName.length - 1; i++) {
+    for (let j = 1; j <= Object.keys(obj).length; j++) {
+      if (obj[j].includes(fullName[i])) {
+        numArr.push(j);
+      } else {
+        null;
+      }
+    }
+  }
+}
 function calculate(e) {
   e.preventDefault();
 }
